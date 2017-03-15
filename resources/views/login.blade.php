a578d35f9c54b71c53a6e07fdb7557f431206761
Muhammad Huda <heihudaa@gmail.com>
2017-03-15 08:07:37 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <title>Login - SIMPUS Rambipuji</title>
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="{{ URL::asset('assets/login/css/style.css') }}">
 </head>
 
 <body>
     <div class="login-form">
         <h1>Simpus Rambi</h1>
         <form method="post" action="/login">
+        {{ csrf_field() }}
             <div class="form-group ">
                 <input type="text" class="form-control" placeholder="Username" name="username" id="username" required="">
                 <i class="fa fa-user"></i>
@@ -18,11 +20,19 @@
                 <input type="password" class="form-control" placeholder="Password" name="password" id="password" required="">
             <i class="fa fa-lock"></i>
             </div>
+            @if (count($errors) > 0)
+            <div class="alert">
+                @foreach ($errors->all() as $error)
+                    {{ $error }}
+                @endforeach
+            </div>
+            @endif
             <button type="submit" class="log-btn" >Log in</button>
         </form>
     
     </div>
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="{{ URL::asset('assets/login/js/index.js') }}"></script>
 
 </body>
 </html>
